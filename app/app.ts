24a00bbf244f20dbf2eff36ef667938eeaddf6da
Valente Meza <kingracoon64@gmail.com>
2017-02-15 10:02:54 -0700

    app.ts added

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import { enableProdMode } from '@angular/core';
 
-enableProdMode();
+//enableProdMode(); enable on demo or when you have confidenc in the site.
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
